1f612822ee36075d4ef2ba0aa2dcd79585bb0ba8
Iryna <kytsenkoiryna33@gmail.com>
2025-09-28 17:34:48 +0300

    SRP

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+class Book
+{
+    public required string Title { get; set; }
+    public required string Author { get; set; }
+}
+
+class BookSaver
+{
+    public void Save(Book book)
+    {
+        File.WriteAllText("book.txt", $"{book.Title} by {book.Author}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Book book = new Book { Title = "1984", Author = "George Orwell" };
+        BookSaver saver = new BookSaver();
+        saver.Save(book);
+        Console.WriteLine("Book saved!");
+    }
+}
+
